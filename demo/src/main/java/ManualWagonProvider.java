aed47081b8d1818a9d1ef230cc9c12b8de9b52d3
Benjamin Bentmann <bentmann@bbm.cservenak.net>
2010-05-27 10:52:29 +0200

    o Extended demo

---
+++ demo/src/main/java/ManualWagonProvider.java
@@ -0,0 +1,24 @@
+import org.apache.maven.repository.wagon.WagonProvider;
+import org.apache.maven.wagon.Wagon;
+import org.apache.maven.wagon.providers.file.FileWagon;
+
+class ManualWagonProvider
+    implements WagonProvider
+{
+
+    public Wagon lookup( String roleHint )
+        throws Exception
+    {
+        if ( "file".equals( roleHint ) )
+        {
+            return new FileWagon();
+        }
+        return null;
+    }
+
+    public void release( Wagon wagon )
+    {
+
+    }
+
+}
--- demo/src/main/java/RepoSys.java
+++ demo/src/main/java/RepoSys.java
@@ -1,6 +1,28 @@
 import java.io.File;
 import java.util.Arrays;
 
+import org.apache.maven.model.building.DefaultModelBuilder;
+import org.apache.maven.model.building.DefaultModelProcessor;
+import org.apache.maven.model.building.ModelBuilder;
+import org.apache.maven.model.composition.DefaultDependencyManagementImporter;
+import org.apache.maven.model.inheritance.DefaultInheritanceAssembler;
+import org.apache.maven.model.interpolation.StringSearchModelInterpolator;
+import org.apache.maven.model.io.DefaultModelReader;
+import org.apache.maven.model.locator.DefaultModelLocator;
+import org.apache.maven.model.management.DefaultDependencyManagementInjector;
+import org.apache.maven.model.management.DefaultPluginManagementInjector;
+import org.apache.maven.model.normalization.DefaultModelNormalizer;
+import org.apache.maven.model.path.DefaultModelPathTranslator;
+import org.apache.maven.model.path.DefaultModelUrlNormalizer;
+import org.apache.maven.model.path.DefaultPathTranslator;
+import org.apache.maven.model.path.DefaultUrlNormalizer;
+import org.apache.maven.model.profile.DefaultProfileInjector;
+import org.apache.maven.model.profile.DefaultProfileSelector;
+import org.apache.maven.model.profile.activation.JdkVersionProfileActivator;
+import org.apache.maven.model.profile.activation.OperatingSystemProfileActivator;
+import org.apache.maven.model.profile.activation.PropertyProfileActivator;
+import org.apache.maven.model.superpom.DefaultSuperPomProvider;
+import org.apache.maven.model.validation.DefaultModelValidator;
 import org.apache.maven.repository.Artifact;
 import org.apache.maven.repository.AuthenticationSelector;
 import org.apache.maven.repository.CollectRequest;
@@ -19,6 +41,16 @@ import org.apache.maven.repository.ProxySelector;
 import org.apache.maven.repository.RemoteRepository;
 import org.apache.maven.repository.RepositorySession;
 import org.apache.maven.repository.RepositorySystem;
+import org.apache.maven.repository.internal.DefaultArtifactDescriptorReader;
+import org.apache.maven.repository.internal.DefaultArtifactResolver;
+import org.apache.maven.repository.internal.DefaultDependencyCollector;
+import org.apache.maven.repository.internal.DefaultDeployer;
+import org.apache.maven.repository.internal.DefaultMetadataResolver;
+import org.apache.maven.repository.internal.DefaultRemoteRepositoryManager;
+import org.apache.maven.repository.internal.DefaultRepositorySystem;
+import org.apache.maven.repository.internal.DefaultUpdateCheckManager;
+import org.apache.maven.repository.internal.DefaultVersionRangeResolver;
+import org.apache.maven.repository.internal.DefaultVersionResolver;
 import org.apache.maven.repository.internal.TrackingLocalRepositoryManager;
 import org.apache.maven.repository.spi.NullLogger;
 import org.apache.maven.repository.util.AndDependencyFilter;
@@ -37,6 +69,7 @@ import org.apache.maven.repository.util.JavaDependencyContextRefiner;
 import org.apache.maven.repository.util.JavaEffectiveScopeCalculator;
 import org.apache.maven.repository.util.OptionalDependencyFilter;
 import org.apache.maven.repository.util.ScopeDependencyFilter;
+import org.apache.maven.repository.wagon.WagonRepositoryConnectorFactory;
 import org.codehaus.plexus.DefaultPlexusContainer;
 
 public class RepoSys
@@ -45,9 +78,8 @@ public class RepoSys
     public static void main( String[] args )
         throws Exception
     {
-        DefaultPlexusContainer container = new DefaultPlexusContainer();
-
-        RepositorySystem repoSystem = container.lookup( RepositorySystem.class );
+        RepositorySystem repoSystem = newManagedSystem();
+        //RepositorySystem repoSystem = newManualSystem();
 
         RepositorySession session = newSession();
 
@@ -58,16 +90,107 @@ public class RepoSys
                                                                              "http://repo1.maven.org/maven2/" ) ) );
         DependencyNode root = repoSystem.collectDependencies( session, collectRequest ).getRoot();
 
-        dump( root, "" );
-
         repoSystem.resolveDependencies( session, root );
-        
+
         Artifact projectOutput = new DefaultArtifact( "test", "test", "", "jar", "0.1-SNAPSHOT" );
-        projectOutput.setFile( new File("/Users/bentmann/tmp/z/pom.xml") );
+        projectOutput.setFile( new File( "/Users/bentmann/tmp/z/pom.xml" ) );
         DeployRequest deployRequest = new DeployRequest();
         deployRequest.addArtifact( projectOutput );
         deployRequest.setRepository( new RemoteRepository( "nexus", "default", "file:///Users/bentmann/tmp/dist-repo/" ) );
         repoSystem.deploy( session, deployRequest );
+
+        System.out.println("============================================================");
+        dump( root, "" );
+    }
+
+    private static RepositorySystem newManagedSystem()
+        throws Exception
+    {
+        return new DefaultPlexusContainer().lookup( RepositorySystem.class );
+    }
+
+    private static RepositorySystem newManualSystem()
+    {
+        DefaultUpdateCheckManager updateCheckMan = new DefaultUpdateCheckManager();
+
+        WagonRepositoryConnectorFactory connectorFactory = new WagonRepositoryConnectorFactory();
+        connectorFactory.setWagonProvider( new ManualWagonProvider() );
+        
+        DefaultRemoteRepositoryManager remoteRepoMan = new DefaultRemoteRepositoryManager();
+        remoteRepoMan.setUpdateCheckManager( updateCheckMan );
+        remoteRepoMan.addRepositoryConnectorFactory( connectorFactory );
+
+        DefaultMetadataResolver metadataResolver = new DefaultMetadataResolver();
+        metadataResolver.setRemoteRepositoryManager( remoteRepoMan );
+        metadataResolver.setUpdateCheckManager( updateCheckMan );
+
+        DefaultVersionResolver versionResolver = new DefaultVersionResolver();
+        versionResolver.setMetadataResolver( metadataResolver );
+
+        DefaultVersionRangeResolver versionRangeResolver = new DefaultVersionRangeResolver();
+        versionRangeResolver.setMetadataResolver( metadataResolver );
+
+        DefaultArtifactResolver artifactResolver = new DefaultArtifactResolver();
+        artifactResolver.setRemoteRepositoryManager( remoteRepoMan );
+        artifactResolver.setUpdateCheckManager( updateCheckMan );
+        artifactResolver.setVersionResolver( versionResolver );
+
+        DefaultArtifactDescriptorReader pomReader = new DefaultArtifactDescriptorReader();
+        pomReader.setArtifactResolver( artifactResolver );
+        pomReader.setRemoteRepositoryManager( remoteRepoMan );
+        pomReader.setModelBuilder( newModelBuilder() );
+
+        DefaultDependencyCollector collector = new DefaultDependencyCollector();
+        collector.setRemoteRepositoryManager( remoteRepoMan );
+        collector.setArtifactDescriptorReader( pomReader );
+        collector.setVersionRangeResolver( versionRangeResolver );
+
+        DefaultDeployer deployer = new DefaultDeployer();
+        deployer.setRemoteRepositoryManager( remoteRepoMan );
+        deployer.setUpdateCheckManager( updateCheckMan );
+
+        DefaultRepositorySystem repoSys = new DefaultRepositorySystem();
+        repoSys.setArtifactDescriptorReader( pomReader );
+        repoSys.setArtifactResolver( artifactResolver );
+        repoSys.setMetadataResolver( metadataResolver );
+        repoSys.setVersionResolver( versionResolver );
+        repoSys.setVersionRangeResolver( versionRangeResolver );
+        repoSys.setDependencyCollector( collector );
+        repoSys.setDeployer( deployer );
+
+        return repoSys;
+    }
+
+    private static ModelBuilder newModelBuilder()
+    {
+        DefaultModelProcessor processor = new DefaultModelProcessor();
+        processor.setModelLocator( new DefaultModelLocator() );
+        processor.setModelReader( new DefaultModelReader() );
+
+        DefaultProfileSelector profileSelector = new DefaultProfileSelector();
+        profileSelector.addProfileActivator( new JdkVersionProfileActivator() );
+        profileSelector.addProfileActivator( new OperatingSystemProfileActivator() );
+        profileSelector.addProfileActivator( new PropertyProfileActivator() );
+
+        DefaultUrlNormalizer urlNormalizer = new DefaultUrlNormalizer();
+        DefaultPathTranslator pathTranslator = new DefaultPathTranslator();
+        
+        DefaultModelBuilder modelBuilder = new DefaultModelBuilder();
+        modelBuilder.setModelProcessor( processor );
+        modelBuilder.setModelValidator( new DefaultModelValidator() );
+        modelBuilder.setModelNormalizer( new DefaultModelNormalizer() );
+        modelBuilder.setModelPathTranslator( new DefaultModelPathTranslator().setPathTranslator( pathTranslator ) );
+        modelBuilder.setModelUrlNormalizer( new DefaultModelUrlNormalizer().setUrlNormalizer( urlNormalizer ) );
+        modelBuilder.setModelInterpolator( new StringSearchModelInterpolator().setPathTranslator( pathTranslator ).setUrlNormalizer( urlNormalizer ) );
+        modelBuilder.setInheritanceAssembler( new DefaultInheritanceAssembler() );
+        modelBuilder.setProfileInjector( new DefaultProfileInjector() );
+        modelBuilder.setProfileSelector( profileSelector );
+        modelBuilder.setSuperPomProvider( new DefaultSuperPomProvider().setModelProcessor( processor ) );
+        modelBuilder.setDependencyManagementImporter( new DefaultDependencyManagementImporter() );
+        modelBuilder.setDependencyManagementInjector( new DefaultDependencyManagementInjector() );
+        modelBuilder.setPluginManagementInjector( new DefaultPluginManagementInjector() );
+
+        return modelBuilder;
     }
 
     private static RepositorySession newSession()
@@ -101,9 +224,12 @@ public class RepoSys
         session.setDependencyFilter( depFilter );
 
         DependencyGraphTransformer transformer =
-            new ChainedDependencyGraphTransformer( new ConflictMarker(), new JavaEffectiveScopeCalculator(),
-                                                   new ClassicVersionConflictResolver(),
-                                                   new JavaDependencyContextRefiner() );
+            new ChainedDependencyGraphTransformer( 
+                                                   new ConflictMarker(), 
+                                                   new JavaEffectiveScopeCalculator(),
+//                                                   new ClassicVersionConflictResolver(),
+                                                   new JavaDependencyContextRefiner() 
+                                                   );
         session.setDependencyGraphTransformer( transformer );
 
         session.setIgnoreInvalidArtifactDescriptor( true );
