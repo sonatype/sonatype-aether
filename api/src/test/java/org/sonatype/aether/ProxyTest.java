sonatype/sonatype-aether

commit 270c890898474330c9fb278adf447a154ee293a8

fixed errors reported by PMD and FindBugs

api/src/main/java/org/sonatype/aether/Dependency.java
@@ -3,13 +3,13 @@ package org.sonatype.aether;
 /*
  * Copyright (c) 2010 Sonatype, Inc. All rights reserved.
  *
- * This program is licensed to you under the Apache License Version 2.0, 
- * and you may not use this file except in compliance with the Apache License Version 2.0. 
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
  * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
  *
- * Unless required by applicable law or agreed to in writing, 
- * software distributed under the Apache License Version 2.0 is distributed on an 
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
  */
 
@@ -21,7 +21,7 @@ import java.util.HashSet;
 /**
  * A dependency to some artifact. <em>Note:</em> Instances of this class are immutable and the exposed mutators return
  * new objects rather than changing the current instance.
- * 
+ *
  * @author Benjamin Bentmann
  */
 public final class Dependency
@@ -39,7 +39,7 @@ public final class Dependency
 
     /**
      * Creates a mandatory dependency on the specified artifact with the given scope.
-     * 
+     *
      * @param artifact The artifact being depended on, may be {@code null}.
      * @param scope The scope of the dependency, may be {@code null}.
      */
@@ -50,7 +50,7 @@ public final class Dependency
 
     /**
      * Creates a dependency on the specified artifact with the given scope.
-     * 
+     *
      * @param artifact The artifact being depended on, may be {@code null}.
      * @param scope The scope of the dependency, may be {@code null}.
      * @param optional A flag whether the dependency is optional or mandatory.
@@ -62,7 +62,7 @@ public final class Dependency
 
     /**
      * Creates a dependency on the specified artifact with the given scope and exclusions.
-     * 
+     *
      * @param artifact The artifact being depended on, may be {@code null}.
      * @param scope The scope of the dependency, may be {@code null}.
      * @param optional A flag whether the dependency is optional or mandatory.
@@ -96,7 +96,7 @@ public final class Dependency
 
     /**
      * Gets the artifact being depended on.
-     * 
+     *
      * @return The artifact, never {@code null}.
      */
     public Artifact getArtifact()
@@ -106,7 +106,7 @@ public final class Dependency
 
     /**
      * Sets the artifact being depended on.
-     * 
+     *
      * @param artifact The artifact, must not be {@code null}.
      * @return The new dependency, never {@code null}.
      */
@@ -121,7 +121,7 @@ public final class Dependency
 
     /**
      * Gets the scope of the dependency. The scope defines in which context this dependency is relevant.
-     * 
+     *
      * @return The scope or an empty string if not set, never {@code null}.
      */
     public String getScope()
@@ -131,13 +131,13 @@ public final class Dependency
 
     /**
      * Sets the scope of the dependency, e.g. "compile".
-     * 
+     *
      * @param scope The scope of the dependency, may be {@code null}.
      * @return The new dependency, never {@code null}.
      */
     public Dependency setScope( String scope )
     {
-        if ( this.scope.equals( scope ) || ( scope == null && this.scope.length() <= 0 ) )
+        if ( this.scope.equals( scope ) || scope == null || this.scope.length() <= 0 )
         {
             return this;
         }
@@ -145,9 +145,9 @@ public final class Dependency
     }
 
     /**
-     * Indicates whether this dependency is optional or not. Optional dependencies can usually be igored during
+     * Indicates whether this dependency is optional or not. Optional dependencies can usually be ignored during
      * transitive dependency resolution.
-     * 
+     *
      * @return {@code true} if the dependency is optional, {@code false} otherwise.
      */
     public boolean isOptional()
@@ -157,7 +157,7 @@ public final class Dependency
 
     /**
      * Sets the optional flag for the dependency.
-     * 
+     *
      * @param optional {@code true} if the dependency is optional, {@code false} if the dependency is mandatory.
      * @return The new dependency, never {@code null}.
      */
@@ -173,7 +173,7 @@ public final class Dependency
     /**
      * Gets the exclusions for this dependency. Exclusions can be used to remove transitive dependencies during
      * resolution.
-     * 
+     *
      * @return The (read-only) exclusions, never {@code null}.
      */
     public Collection<Exclusion> getExclusions()
@@ -183,13 +183,13 @@ public final class Dependency
 
     /**
      * Sets the exclusions for the dependency.
-     * 
+     *
      * @param exclusions The exclusions, may be {@code null}.
      * @return The new dependency, never {@code null}.
      */
     public Dependency setExclusions( Collection<Exclusion> exclusions )
     {
-        if ( getExclusions().equals( exclusions ) || ( exclusions == null && this.exclusions.length <= 0 ) )
+        if ( getExclusions().equals( exclusions ) || exclusions == null || this.exclusions.length <= 0 )
         {
             return this;
         }

api/src/main/java/org/sonatype/aether/Proxy.java
@@ -3,20 +3,20 @@ package org.sonatype.aether;
 /*
  * Copyright (c) 2010 Sonatype, Inc. All rights reserved.
  *
- * This program is licensed to you under the Apache License Version 2.0, 
- * and you may not use this file except in compliance with the Apache License Version 2.0. 
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
  * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
  *
- * Unless required by applicable law or agreed to in writing, 
- * software distributed under the Apache License Version 2.0 is distributed on an 
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
  */
 
 /**
  * A proxy to use for connections to a repository. <em>Note:</em> Instances of this class are immutable and the exposed
  * mutators return new objects rather than changing the current instance.
- * 
+ *
  * @author Benjamin Bentmann
  */
 public final class Proxy
@@ -32,7 +32,7 @@ public final class Proxy
 
     /**
      * Creates a new proxy with the specified properties.
-     * 
+     *
      * @param type The type of the proxy, e.g. "http", may be {@code null}.
      * @param host The host of the proxy, may be {@code null}.
      * @param port The port of the proxy.
@@ -48,7 +48,7 @@ public final class Proxy
 
     /**
      * Gets the type of this proxy.
-     * 
+     *
      * @return The type of this proxy, never {@code null}.
      */
     public String getType()
@@ -58,13 +58,13 @@ public final class Proxy
 
     /**
      * Sets the type of the proxy.
-     * 
+     *
      * @param type The type of the proxy, e.g. "http", may be {@code null}.
      * @return The new proxy, never {@code null}.
      */
     public Proxy setType( String type )
     {
-        if ( this.type.equals( type ) || ( type == null && this.type.length() <= 0 ) )
+        if ( this.type.equals( type ) || type == null || this.type.length() <= 0 )
         {
             return this;
         }
@@ -73,7 +73,7 @@ public final class Proxy
 
     /**
      * Gets the host for this proxy.
-     * 
+     *
      * @return The host for this proxy, never {@code null}.
      */
     public String getHost()
@@ -83,13 +83,13 @@ public final class Proxy
 
     /**
      * Sets the host of the proxy.
-     * 
+     *
      * @param host The host of the proxy, may be {@code null}.
      * @return The new proxy, never {@code null}.
      */
     public Proxy setHost( String host )
     {
-        if ( this.host.equals( host ) || ( host == null && this.host.length() <= 0 ) )
+        if ( this.host.equals( host ) || host == null || this.host.length() <= 0 )
         {
             return this;
         }
@@ -98,7 +98,7 @@ public final class Proxy
 
     /**
      * Gets the port number for this proxy.
-     * 
+     *
      * @return The port number for this proxy.
      */
     public int getPort()
@@ -108,7 +108,7 @@ public final class Proxy
 
     /**
      * Sets the port number for the proxy.
-     * 
+     *
      * @param port The port number for the proxy.
      * @return The new proxy, never {@code null}.
      */
@@ -123,7 +123,7 @@ public final class Proxy
 
     /**
      * Gets the authentication to use for the proxy connection.
-     * 
+     *
      * @return The authentication to use or {@code null} if none.
      */
     public Authentication getAuthentication()
@@ -133,7 +133,7 @@ public final class Proxy
 
     /**
      * Sets the authentication to use for the proxy connection.
-     * 
+     *
      * @param auth The authentication to use, may be {@code null}.
      * @return The new proxy, never {@code null}.
      */

impl/src/main/java/org/sonatype/aether/impl/internal/DefaultVersionRangeResolver.java
@@ -3,13 +3,13 @@ package org.sonatype.aether.impl.internal;
 /*
  * Copyright (c) 2010 Sonatype, Inc. All rights reserved.
  *
- * This program is licensed to you under the Apache License Version 2.0, 
- * and you may not use this file except in compliance with the Apache License Version 2.0. 
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
  * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
  *
- * Unless required by applicable law or agreed to in writing, 
- * software distributed under the Apache License Version 2.0 is distributed on an 
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
  */
 
@@ -122,15 +122,15 @@ public class DefaultVersionRangeResolver
                 getVersions( session, result, request, getNature( session, versionConstraint.getRanges() ) );
 
             List<Version> versions = new ArrayList<Version>();
-            for ( String v : versionIndex.keySet() )
+            for ( Map.Entry<String, ArtifactRepository> v : versionIndex.entrySet() )
             {
                 try
                 {
-                    Version ver = versionScheme.parseVersion( v );
+                    Version ver = versionScheme.parseVersion( v.getKey() );
                     if ( versionConstraint.containsVersion( ver ) )
                     {
                         versions.add( ver );
-                        result.setRepository( ver, versionIndex.get( v ) );
+                        result.setRepository( ver, v.getValue() );
                     }
                 }
                 catch ( InvalidVersionException e )

commit d21dcea514eca06132cde70ced822b998a1dc77e

o Fixed bugs introduced in 270c8908

api/src/main/java/org/sonatype/aether/Dependency.java
@@ -137,7 +137,7 @@ public final class Dependency
      */
     public Dependency setScope( String scope )
     {
-        if ( this.scope.equals( scope ) || scope == null || this.scope.length() <= 0 )
+        if ( this.scope.equals( scope ) || ( scope == null && this.scope.length() <= 0 ) )
         {
             return this;
         }
@@ -189,7 +189,7 @@ public final class Dependency
      */
     public Dependency setExclusions( Collection<Exclusion> exclusions )
     {
-        if ( getExclusions().equals( exclusions ) || exclusions == null || this.exclusions.length <= 0 )
+        if ( getExclusions().equals( exclusions ) || ( exclusions == null && this.exclusions.length <= 0 ) )
         {
             return this;
         }

api/src/main/java/org/sonatype/aether/Proxy.java
@@ -16,7 +16,7 @@ package org.sonatype.aether;
 /**
  * A proxy to use for connections to a repository. <em>Note:</em> Instances of this class are immutable and the exposed
  * mutators return new objects rather than changing the current instance.
- *
+ * 
  * @author Benjamin Bentmann
  */
 public final class Proxy
@@ -32,7 +32,7 @@ public final class Proxy
 
     /**
      * Creates a new proxy with the specified properties.
-     *
+     * 
      * @param type The type of the proxy, e.g. "http", may be {@code null}.
      * @param host The host of the proxy, may be {@code null}.
      * @param port The port of the proxy.
@@ -48,7 +48,7 @@ public final class Proxy
 
     /**
      * Gets the type of this proxy.
-     *
+     * 
      * @return The type of this proxy, never {@code null}.
      */
     public String getType()
@@ -58,13 +58,13 @@ public final class Proxy
 
     /**
      * Sets the type of the proxy.
-     *
+     * 
      * @param type The type of the proxy, e.g. "http", may be {@code null}.
      * @return The new proxy, never {@code null}.
      */
     public Proxy setType( String type )
     {
-        if ( this.type.equals( type ) || type == null || this.type.length() <= 0 )
+        if ( this.type.equals( type ) || ( type == null && this.type.length() <= 0 ) )
         {
             return this;
         }
@@ -73,7 +73,7 @@ public final class Proxy
 
     /**
      * Gets the host for this proxy.
-     *
+     * 
      * @return The host for this proxy, never {@code null}.
      */
     public String getHost()
@@ -83,13 +83,13 @@ public final class Proxy
 
     /**
      * Sets the host of the proxy.
-     *
+     * 
      * @param host The host of the proxy, may be {@code null}.
      * @return The new proxy, never {@code null}.
      */
     public Proxy setHost( String host )
     {
-        if ( this.host.equals( host ) || host == null || this.host.length() <= 0 )
+        if ( this.host.equals( host ) || ( host == null && this.host.length() <= 0 ) )
         {
             return this;
         }
@@ -98,7 +98,7 @@ public final class Proxy
 
     /**
      * Gets the port number for this proxy.
-     *
+     * 
      * @return The port number for this proxy.
      */
     public int getPort()
@@ -108,7 +108,7 @@ public final class Proxy
 
     /**
      * Sets the port number for the proxy.
-     *
+     * 
      * @param port The port number for the proxy.
      * @return The new proxy, never {@code null}.
      */
@@ -123,7 +123,7 @@ public final class Proxy
 
     /**
      * Gets the authentication to use for the proxy connection.
-     *
+     * 
      * @return The authentication to use or {@code null} if none.
      */
     public Authentication getAuthentication()
@@ -133,7 +133,7 @@ public final class Proxy
 
     /**
      * Sets the authentication to use for the proxy connection.
-     *
+     * 
      * @param auth The authentication to use, may be {@code null}.
      * @return The new proxy, never {@code null}.
      */

api/src/test/java/org/sonatype/aether/DependencyTest.java
@@ -1,0 +1,60 @@
+package org.sonatype.aether;
+
+/*
+ * Copyright (c) 2010 Sonatype, Inc. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0, 
+ * and you may not use this file except in compliance with the Apache License Version 2.0. 
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, 
+ * software distributed under the Apache License Version 2.0 is distributed on an 
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.junit.Test;
+
+/**
+ * @author Benjamin Bentmann
+ */
+public class DependencyTest
+{
+
+    @Test
+    public void testSetScope()
+    {
+        Dependency d1 = new Dependency( new DefaultArtifact( "gid:aid:ver" ), "compile" );
+
+        Dependency d2 = d1.setScope( null );
+        assertNotSame( d2, d1 );
+        assertEquals( "", d2.getScope() );
+
+        Dependency d3 = d1.setScope( "test" );
+        assertNotSame( d3, d1 );
+        assertEquals( "test", d3.getScope() );
+    }
+
+    @Test
+    public void testSetExclusions()
+    {
+        Dependency d1 =
+            new Dependency( new DefaultArtifact( "gid:aid:ver" ), "compile", false,
+                            Collections.singleton( new Exclusion( "g", "a", "c", "e" ) ) );
+
+        Dependency d2 = d1.setExclusions( null );
+        assertNotSame( d2, d1 );
+        assertEquals( 0, d2.getExclusions().size() );
+
+        Dependency d3 =
+            d1.setExclusions( Arrays.asList( new Exclusion( "g", "a", "c", "e" ), new Exclusion( "g", "a", "c", "e" ) ) );
+        assertNotSame( d3, d1 );
+        assertEquals( 2, d3.getExclusions().size() );
+    }
+
+}

api/src/test/java/org/sonatype/aether/ProxyTest.java
@@ -1,0 +1,64 @@
+package org.sonatype.aether;
+
+/*
+ * Copyright (c) 2010 Sonatype, Inc. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0, 
+ * and you may not use this file except in compliance with the Apache License Version 2.0. 
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, 
+ * software distributed under the Apache License Version 2.0 is distributed on an 
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Benjamin Bentmann
+ */
+public class ProxyTest
+{
+
+    @Test
+    public void testSetHost()
+    {
+        Proxy p1 = new Proxy( "type", "host", 80, null );
+
+        Proxy p2 = p1.setHost( null );
+        assertNotSame( p2, p1 );
+        assertEquals( "", p2.getHost() );
+
+        Proxy p3 = p1.setHost( "HOST" );
+        assertNotSame( p3, p1 );
+        assertEquals( "HOST", p3.getHost() );
+    }
+
+    @Test
+    public void testSetType()
+    {
+        Proxy p1 = new Proxy( "type", "host", 80, null );
+
+        Proxy p2 = p1.setType( null );
+        assertNotSame( p2, p1 );
+        assertEquals( "", p2.getType() );
+
+        Proxy p3 = p1.setType( "TYPE" );
+        assertNotSame( p3, p1 );
+        assertEquals( "TYPE", p3.getType() );
+    }
+
+    @Test
+    public void testSetPort()
+    {
+        Proxy p1 = new Proxy( "type", "host", 80, null );
+
+        Proxy p2 = p1.setPort( 8080 );
+        assertNotSame( p2, p1 );
+        assertEquals( 8080, p2.getPort() );
+    }
+
+}