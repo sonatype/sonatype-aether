e3743327bdd5466a2f215d89ff28b5f0cd894a0b
Benjamin Hanzelmann <ben@nabcos.de>
2010-08-20 17:15:10 +0200

    o FileUtil copy/deleteDir

--- aether-test-util/src/main/java/org/sonatype/aether/test/util/FileUtil.java
+++ aether-test-util/src/main/java/org/sonatype/aether/test/util/FileUtil.java
@@ -14,8 +14,14 @@ package org.sonatype.aether.test.util;
  */
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+import org.sonatype.aether.TransferCancelledException;
 
 public class FileUtil
 {
@@ -23,14 +29,14 @@ public class FileUtil
     public static File createTempFile( byte[] pattern, int repeat )
         throws IOException
     {
-    
+
         File tmpFile = null;
         FileOutputStream out = null;
         try
         {
             tmpFile = File.createTempFile( "aether-test-util-", ".data" );
             tmpFile.deleteOnExit();
-    
+
             out = new FileOutputStream( tmpFile );
             for ( int i = 0; i < repeat; i++ )
             {
@@ -44,8 +50,81 @@ public class FileUtil
                 out.close();
             }
         }
-    
+
         return tmpFile;
+    }
+
+    public static long copy( File src, File target )
+        throws IOException
+    {
+        if ( src == null || target == null )
+        {
+            throw new IllegalArgumentException( String.format( "src and target may not be null: '%s' -> '%s'", src,
+                                                               target ) );
+        }
+
+        FileChannel in = null;
+        FileChannel out = null;
+        FileInputStream inStream = null;
+        FileOutputStream outStream = null;
+
+        long total = 0;
+        try
+        {
+            inStream = new FileInputStream( src );
+            in = inStream.getChannel();
+            outStream = new FileOutputStream( target );
+            out = outStream.getChannel();
+            long count = 20000L;
+            ByteBuffer buf = ByteBuffer.allocate( (int) count );
+
+            buf.clear();
+            int transferred;
+            while ( ( transferred = in.read( buf ) ) >= 0 || buf.position() != 0 )
+            {
+                total += transferred;
+
+                buf.flip();
+                out.write( buf );
+                buf.compact();
+            }
+            buf.flip();
+            while ( buf.hasRemaining() )
+            {
+                out.write( buf );
+            }
+        }
+        finally
+        {
+            if ( inStream != null )
+                inStream.close();
+            if ( in != null )
+                in.close();
+            if ( outStream != null )
+                outStream.close();
+            if ( out != null )
+                out.close();
+        }
+
+        return total;
+    }
+
+    public static boolean deleteDir( File dir )
+    {
+        if ( dir.isDirectory() )
+        {
+            String[] children = dir.list();
+            for ( int i = 0; i < children.length; i++ )
+            {
+                boolean success = deleteDir( new File( dir, children[i] ) );
+                if ( !success )
+                {
+                    return false;
+                }
+            }
+        }
+    
+        return dir.delete();
     }
 
 }
