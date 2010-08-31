98add7a30552ccf0578c7a4d174edcc01c0ad3d7
Benjamin Hanzelmann <ben@nabcos.de>
2010-08-31 12:14:34 +0200

    o test for mkdirs() concurrency bug

--- aether-connector-file/src/test/java/org/sonatype/aether/connector/file/FileRepositoryConnectorTest.java
+++ aether-connector-file/src/test/java/org/sonatype/aether/connector/file/FileRepositoryConnectorTest.java
@@ -18,7 +18,10 @@ import static org.junit.Assert.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collection;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.sonatype.aether.metadata.Metadata;
 import org.sonatype.aether.spi.connector.ArtifactDownload;
@@ -27,6 +30,7 @@ import org.sonatype.aether.spi.connector.MetadataDownload;
 import org.sonatype.aether.spi.connector.MetadataUpload;
 import org.sonatype.aether.spi.connector.RepositoryConnectorFactory;
 import org.sonatype.aether.spi.connector.Transfer;
+import org.sonatype.aether.spi.connector.Transfer.State;
 import org.sonatype.aether.test.util.FileUtil;
 import org.sonatype.aether.test.util.connector.ConnectorTestContext;
 import org.sonatype.aether.test.util.connector.TransferEventTester;
@@ -37,14 +41,60 @@ import org.sonatype.aether.util.metadata.DefaultMetadata;
 public class FileRepositoryConnectorTest
 {
 
+    private FileRepositoryConnector connector;
+
+    private ConnectorTestContext ctx;
+
+    @Before
+    public void setup()
+        throws NoRepositoryConnectorException
+    {
+        ctx = TransferEventTester.setupTestContext();
+        connector = new FileRepositoryConnector( ctx.getSession(), ctx.getRepository() );
+    }
+
+    @After
+    public void cleanup()
+    {
+        FileUtil.deleteDir( new File( PathUtils.basedir( ctx.getRepository().getUrl() ) ) );
+    }
+
+    @Test
+    public void testMkdirConcurrencyBug()
+        throws IOException
+    {
+        int i = 1;
+        File tmpFile = FileUtil.createTempFile( "mkdirsBug" );
+        ArtifactUpload artUp =
+            new ArtifactUpload( new DefaultArtifact( "testGroup", "testArtifact", "jar", i + "-test" ), tmpFile );
+        MetadataUpload metaUp =
+            new MetadataUpload( new DefaultMetadata( "testGroup", "testArtifact", i + "-test", "maven-metadata.xml",
+                                                     Metadata.Nature.RELEASE_OR_SNAPSHOT ), tmpFile );
+        
+        for ( int j = 0; j < 1000; j++ )
+        {
+
+            Collection<ArtifactUpload> artUps = Arrays.asList( artUp );
+            Collection<MetadataUpload> metaUps = Arrays.asList( metaUp );
+
+            connector.put( artUps, metaUps );
+
+            assertNull( artUp.getException() );
+            assertNull( metaUp.getException() );
+            assertEquals( State.DONE, artUp.getState() );
+            assertEquals( State.DONE, metaUp.getState() );
+
+            cleanup();
+        }
+
+    }
+    
     @Test
     public void testBlocking()
         throws NoRepositoryConnectorException, IOException
     {
-        ConnectorTestContext ctx = TransferEventTester.setupTestContext();
-        FileRepositoryConnector connector = new FileRepositoryConnector( ctx.getSession(), ctx.getRepository() );
 
-        int count = 10;
+        int count = 1000;
 
         byte[] pattern = "tmpFile".getBytes();
         File tmpFile = FileUtil.createTempFile( pattern, 10000 );
@@ -86,6 +136,8 @@ public class FileRepositoryConnectorTest
 
         // this should block until all transfers are done - racing condition, better way to test this?
         connector.get( Arrays.asList( artDowns ), Arrays.asList( metaDowns ) );
+
+        verifyDone( artDowns, metaDowns );
     }
 
     private void verifyDone( Transfer[] artUps, Transfer[] metaUps )
