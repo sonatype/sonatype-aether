aced6e220b93b284181e0a804490b06e879c6dc8
Benjamin Hanzelmann <ben@nabcos.de>
2010-09-30 09:18:31 +0200

    o Test utilities: add assertContent(String,File)

--- aether-test-util/src/main/java/org/sonatype/aether/test/util/TestFileUtils.java
+++ aether-test-util/src/main/java/org/sonatype/aether/test/util/TestFileUtils.java
@@ -190,4 +190,16 @@ public class TestFileUtils
         Assert.assertArrayEquals( expected, getContent( file ) );
     }
 
+    public static void assertContent( String expected, File file )
+        throws IOException
+    {
+        byte[] content = getContent( file );
+        String msg = new String( content, "UTF-8" );
+        if ( msg.length() > 10 )
+        {
+            msg = msg.substring( 0, 10 ) + "...";
+        }
+        Assert.assertArrayEquals( "content was '" + msg + "'\n", expected.getBytes( "UTF-8" ), content );
+    }
+
 }
