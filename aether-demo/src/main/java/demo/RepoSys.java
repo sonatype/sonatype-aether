b08853c1ea05a620cd113e662d65e9cd5d29b07f
Benjamin Bentmann <benjamin.bentmann@udo.edu>
2010-11-04 20:27:19 +0100

    o Extended demo to excersise query of available artifact versions

--- aether-demo/src/main/java/demo/RepoSys.java
+++ aether-demo/src/main/java/demo/RepoSys.java
@@ -18,26 +18,26 @@ import java.io.File;
 import org.apache.maven.repository.internal.DefaultServiceLocator;
 import org.apache.maven.repository.internal.MavenRepositorySystemSession;
 import org.codehaus.plexus.DefaultPlexusContainer;
+import org.sonatype.aether.RepositoryException;
 import org.sonatype.aether.RepositorySystem;
 import org.sonatype.aether.RepositorySystemSession;
 import org.sonatype.aether.repository.LocalRepository;
 import org.sonatype.aether.repository.RemoteRepository;
-import org.sonatype.aether.resolution.ArtifactResolutionException;
+import org.sonatype.aether.resolution.VersionRangeRequest;
+import org.sonatype.aether.resolution.VersionRangeResult;
 import org.sonatype.aether.spi.connector.RepositoryConnectorFactory;
 import org.sonatype.aether.artifact.Artifact;
 import org.sonatype.aether.collection.CollectRequest;
-import org.sonatype.aether.collection.DependencyCollectionException;
 import org.sonatype.aether.connector.wagon.WagonProvider;
 import org.sonatype.aether.connector.wagon.WagonRepositoryConnectorFactory;
 import org.sonatype.aether.deployment.DeployRequest;
-import org.sonatype.aether.deployment.DeploymentException;
 import org.sonatype.aether.graph.Dependency;
 import org.sonatype.aether.graph.DependencyNode;
 import org.sonatype.aether.installation.InstallRequest;
-import org.sonatype.aether.installation.InstallationException;
 import org.sonatype.aether.util.artifact.DefaultArtifact;
 import org.sonatype.aether.util.artifact.SubArtifact;
 import org.sonatype.aether.util.graph.PreorderNodeListGenerator;
+import org.sonatype.aether.version.Version;
 
 public class RepoSys
 {
@@ -66,13 +66,34 @@ public class RepoSys
     }
 
     private static void playTheDanceBaby( RepositorySystem repoSystem )
-        throws DependencyCollectionException, ArtifactResolutionException, InstallationException, DeploymentException
+        throws RepositoryException
     {
         RepositorySystemSession session = newSession( repoSystem );
 
         Dependency dependency =
             new Dependency( new DefaultArtifact( "org.apache.maven:maven-aether-provider:3.0" ), "runtime" );
         RemoteRepository central = new RemoteRepository( "central", "default", "http://repo1.maven.org/maven2/" );
+
+        /*
+         * Resolve a version range, thereby finding all available/matchin versions
+         */
+
+        VersionRangeRequest rangeRequest = new VersionRangeRequest();
+        rangeRequest.setArtifact( new DefaultArtifact( "org.apache.maven:maven-model:[0,)" ) );
+        rangeRequest.addRepository( central );
+
+        VersionRangeResult rangeResult = repoSystem.resolveVersionRange( session, rangeRequest );
+
+        System.out.println( "------------------------------------------------------------" );
+        System.out.println( "Version results" );
+        for ( Version version : rangeResult.getVersions() )
+        {
+            System.out.println( version );
+        }
+
+        /*
+         * Collect and resolve all transitive dependencies
+         */
 
         CollectRequest collectRequest = new CollectRequest();
         collectRequest.setRoot( dependency );
@@ -98,9 +119,17 @@ public class RepoSys
         Artifact projectPom = new SubArtifact( projectOutput, "", "pom" );
         projectPom = projectPom.setFile( new File( "pom.xml" ) );
 
+        /*
+         * Install artifacts to the local repository
+         */
+
         InstallRequest installRequest = new InstallRequest();
         installRequest.addArtifact( projectOutput ).addArtifact( projectPom );
         repoSystem.install( session, installRequest );
+
+        /*
+         * Deploy artifacts to a remote repository
+         */
 
         DeployRequest deployRequest = new DeployRequest();
         deployRequest.addArtifact( projectOutput ).addArtifact( projectPom );
