a265f1af2bd4edc664503b815360618d0841810c
Benjamin Bentmann <benjamin.bentmann@udo.edu>
2010-11-04 23:40:47 +0100

    o Relicensed ASL to EPL

--- aether-api/src/main/java/org/sonatype/aether/collection/DependencyCollectionContext.java
+++ aether-api/src/main/java/org/sonatype/aether/collection/DependencyCollectionContext.java
@@ -1,17 +1,12 @@
 package org.sonatype.aether.collection;
 
-/*
- * Copyright (c) 2010 Sonatype, Inc. All rights reserved.
- *
- * This program is licensed to you under the Apache License Version 2.0,
- * and you may not use this file except in compliance with the Apache License Version 2.0.
- * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the Apache License Version 2.0 is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
- */
+/*******************************************************************************
+ * Copyright (c) 2010 Sonatype, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
 
 import java.util.List;
 
