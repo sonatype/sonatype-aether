779d7ff413f3a2d5ab0c0340ca2bfa6f6e50a335
jfarcand <jfarcand@apache.org>
2010-12-17 18:17:30 -0500

    Re-add IDEA removed import

--- aether-connector-asynchttpclient/src/main/java/org/sonatype/aether/connector/async/AsyncRepositoryConnector.java
+++ aether-connector-asynchttpclient/src/main/java/org/sonatype/aether/connector/async/AsyncRepositoryConnector.java
@@ -58,6 +58,7 @@ import org.sonatype.aether.util.listener.DefaultTransferResource;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
