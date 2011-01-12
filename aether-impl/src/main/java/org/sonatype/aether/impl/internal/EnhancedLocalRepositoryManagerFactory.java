502b51a5f551629a7483ce133987f262c53b5659
Benjamin Bentmann <benjamin.bentmann@udo.edu>
2011-01-12 16:44:59 +0100

    o Updated copyright year

--- aether-impl/src/main/java/org/sonatype/aether/impl/internal/EnhancedLocalRepositoryManagerFactory.java
+++ aether-impl/src/main/java/org/sonatype/aether/impl/internal/EnhancedLocalRepositoryManagerFactory.java
@@ -1,7 +1,7 @@
 package org.sonatype.aether.impl.internal;
 
 /*
- * Copyright (c) 2010 Sonatype, Inc. All rights reserved.
+ * Copyright (c) 2010-2011 Sonatype, Inc. All rights reserved.
  *
  * This program is licensed to you under the Apache License Version 2.0, 
  * and you may not use this file except in compliance with the Apache License Version 2.0. 
