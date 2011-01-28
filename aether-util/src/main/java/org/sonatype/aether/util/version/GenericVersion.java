2d1f6a2b9557c55e98efba56ee02d43105ef1250
Benjamin Bentmann <benjamin.bentmann@udo.edu>
2011-01-28 17:10:39 +0100

    o Double licensed to EPL+AL

--- aether-util/src/main/java/org/sonatype/aether/util/version/GenericVersion.java
+++ aether-util/src/main/java/org/sonatype/aether/util/version/GenericVersion.java
@@ -1,11 +1,15 @@
 package org.sonatype.aether.util.version;
 
 /*******************************************************************************
- * Copyright (c) 2010 Sonatype, Inc.
+ * Copyright (c) 2010-2011 Sonatype, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * and Apache License v2.0 which accompanies this distribution.
+ * The Eclipse Public License is available at
+ *   http://www.eclipse.org/legal/epl-v10.html
+ * The Apache License v2.0 is available at
+ *   http://www.apache.org/licenses/LICENSE-2.0.html
+ * You may elect to redistribute this code under either of these licenses.
  *******************************************************************************/
 
 import java.math.BigInteger;
