93cbdd06bd8ed483fca7ba22d8761fe311a921e0
Benjamin Bentmann <benjamin.bentmann@udo.edu>
2011-09-14 17:09:08 +0200

    o Fixed logger setup

--- aether-impl/src/main/java/org/sonatype/aether/impl/internal/EnhancedLocalRepositoryManagerFactory.java
+++ aether-impl/src/main/java/org/sonatype/aether/impl/internal/EnhancedLocalRepositoryManagerFactory.java
@@ -9,10 +9,15 @@ package org.sonatype.aether.impl.internal;
  *******************************************************************************/
 
 import org.codehaus.plexus.component.annotations.Component;
+import org.codehaus.plexus.component.annotations.Requirement;
 import org.sonatype.aether.repository.LocalRepository;
 import org.sonatype.aether.repository.LocalRepositoryManager;
 import org.sonatype.aether.repository.NoLocalRepositoryManagerException;
 import org.sonatype.aether.spi.localrepo.LocalRepositoryManagerFactory;
+import org.sonatype.aether.spi.locator.Service;
+import org.sonatype.aether.spi.locator.ServiceLocator;
+import org.sonatype.aether.spi.log.Logger;
+import org.sonatype.aether.spi.log.NullLogger;
 
 /**
  * Creates {@link EnhancedLocalRepositoryManager}s for repository types {@code "default"} or {@code "" (automatic)}.
@@ -21,20 +26,34 @@ import org.sonatype.aether.spi.localrepo.LocalRepositoryManagerFactory;
  */
 @Component( role = LocalRepositoryManagerFactory.class, hint = "enhanced" )
 public class EnhancedLocalRepositoryManagerFactory
-    implements LocalRepositoryManagerFactory
+    implements LocalRepositoryManagerFactory, Service
 {
+
+    @Requirement
+    private Logger logger = NullLogger.INSTANCE;
 
     public LocalRepositoryManager newInstance( LocalRepository repository )
         throws NoLocalRepositoryManagerException
     {
         if ( "".equals( repository.getContentType() ) || "default".equals( repository.getContentType() ) )
         {
-            return new EnhancedLocalRepositoryManager( repository.getBasedir() );
+            return new EnhancedLocalRepositoryManager( repository.getBasedir() ).setLogger( logger );
         }
         else
         {
             throw new NoLocalRepositoryManagerException( repository );
         }
+    }
+
+    public void initService( ServiceLocator locator )
+    {
+        setLogger( locator.getService( Logger.class ) );
+    }
+
+    public EnhancedLocalRepositoryManagerFactory setLogger( Logger logger )
+    {
+        this.logger = ( logger != null ) ? logger : NullLogger.INSTANCE;
+        return this;
     }
 
     public int getPriority()
